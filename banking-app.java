7bb1f4be0c16155b639558464fa265282a830a23
sandraimmaculate <140578526+sandraimmaculate@users.noreply.github.com>
2025-11-01 19:17:00 +0530

    Create banking-app.java

---
+++ banking-app.java
@@ -0,0 +1,7 @@
+package com.bankingmanagementwithmvc;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Banking Management Application - CI/CD Demo!");
+    }
+}
